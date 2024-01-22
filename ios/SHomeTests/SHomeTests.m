6b2a8535bf084f349534405e9cffa2b383970952
baoeggroup <bao@e-ggroup.com>
2024-01-22 14:57:42 +0700

    "update"

--- ios/SHomeTests/SHomeTests.m
+++ ios/SHomeTests/SHomeTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface TravelioTests : XCTestCase
+@interface SHomeTests : XCTestCase
 
 @end
 
-@implementation TravelioTests
+@implementation SHomeTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
