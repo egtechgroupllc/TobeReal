1ce2218c71f06636fa7690e719d2d4d9e81672b0
mac <mac@iMac-cua-mac.local>
2024-01-22 21:03:42 +0700

    upadte

--- src/screen/Explore/DetailAccommodationScreen.js
+++ src/screen/Explore/DetailAccommodationScreen.js
@@ -29,7 +29,8 @@ export default function DetailAccommodationScreen() {
   const [tabBarHeight, setTabBarHeight] = useState(0);
   const [dataSourceCords, setDataSourceCords] = useState([]);
   const [indexSelect, setIndexSelect] = useState(0);
-  const [isSelect, setIsSelect] = useState(false);
+  const [isSelect, setIsSelect] = useState(true);
+
   const scrollOffsetY = useRef(new Animated.Value(0)).current;
   const scrollRef = useRef();
 
@@ -39,7 +40,7 @@ export default function DetailAccommodationScreen() {
         key={key}
         onLayout={e => {
           const layout = e.nativeEvent.layout;
-          const heightHearBar = Header_Max_Height - scale(50);
+          const heightHearBar = Header_Max_Height - 50;
           dataSourceCords[key] = Math.round(layout.y + heightHearBar);
           setDataSourceCords(dataSourceCords);
         }}>
@@ -47,56 +48,50 @@ export default function DetailAccommodationScreen() {
       </View>
     );
   };
+
   const handleScroll = Animated.event(
     [{nativeEvent: {contentOffset: {y: scrollOffsetY}}}],
     {
       useNativeDriver: true,
       listener: event => {
         const offsetY = Math.floor(event.nativeEvent.contentOffset.y);
-        moveNavigateBar(offsetY);
+
+        if (isSelect) moveNavigateBar(offsetY);
       },
     },
   );
 
   const moveNavigateBar = offsetY => {
-    if (offsetY <= dataSourceCords[0]) {
-      setIndexSelect(0);
-    } else if (dataSourceCords[0] < offsetY && offsetY <= dataSourceCords[1]) {
-      setIndexSelect(1);
-    } else if (dataSourceCords[1] < offsetY && offsetY <= dataSourceCords[2]) {
-      setIndexSelect(2);
-    } else if (dataSourceCords[2] < offsetY && offsetY <= dataSourceCords[3]) {
-      setIndexSelect(3);
-    } else {
-      setIndexSelect(4);
+    switch (true) {
+      case offsetY < dataSourceCords[1]:
+        setIndexSelect(0);
+        break;
+      case dataSourceCords[1] <= offsetY && offsetY < dataSourceCords[2]:
+        setIndexSelect(1);
+        break;
+      case dataSourceCords[2] <= offsetY && offsetY < dataSourceCords[3]:
+        setIndexSelect(2);
+        break;
+      case dataSourceCords[3] <= offsetY && offsetY < dataSourceCords[4]:
+        setIndexSelect(3);
+        break;
+      case dataSourceCords[4] <= offsetY:
+        setIndexSelect(4);
+        break;
     }
-
-    // switch (offsetY) {
-    //   case offsetY <= dataSourceCords[0]:
-    //     setIndexSelect(0);
-    //     break;
-    //   case dataSourceCords[0] < offsetY && offsetY <= dataSourceCords[1]:
-    //     setIndexSelect(1);
-    //     break;
-    //   case dataSourceCords[1] < offsetY && offsetY <= dataSourceCords[2]:
-    //     setIndexSelect(2);
-    //     break;
-    //   case dataSourceCords[2] < offsetY && offsetY <= dataSourceCords[3]:
-    //     setIndexSelect(3);
-    //     break;
-    //   case dataSourceCords[3] < offsetY && offsetY <= dataSourceCords[4]:
-    //     setIndexSelect(4);
-    //     break;
-    // }
   };
 
   const selectScrollHandler = async value => {
-    setIsSelect(true);
+    setIsSelect(false);
 
-    await scrollRef.current?.scrollTo({
+    scrollRef.current?.scrollTo({
       y: dataSourceCords[value],
       animated: true,
     });
+
+    setTimeout(() => {
+      setIsSelect(true);
+    }, 500);
   };
 
   return (
