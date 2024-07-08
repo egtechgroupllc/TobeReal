6becb0f1801ecc77b255dffd659c9cbc8e6ef454
baoeggroup <138464464+baoeggroup@users.noreply.github.com>
2024-07-08 08:44:37 +0700

    Update

--- src/Model/url.js
+++ src/Model/url.js
@@ -1,4 +1,4 @@
 // export const baseUrl = 'https://shome.largeswap.com';
-export const baseUrl = 'http://192.168.0.197:3000';
-// export const baseUrl = 'https://api.saveloka.com';
+// export const baseUrl = 'http://192.168.0.197:3000';
+export const baseUrl = 'https://api.saveloka.com';
 export const KEY_MAP = 'AIzaSyA0p4aVomqYcT6mgNv1h169myUhT7mNE28';
