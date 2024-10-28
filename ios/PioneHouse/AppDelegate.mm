cac79be294843b997a2f89df5e3d1700f8bad610
baoeggroup <bao@e-ggroup.com>
2024-10-28 16:04:06 +0700

    "new"

--- ios/PioneHouse/AppDelegate.mm
+++ ios/PioneHouse/AppDelegate.mm
@@ -10,7 +10,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   
-  self.moduleName = @"Saveloka";
+  self.moduleName = @"Pione House";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
--- ios/PioneHouseTests/PioneHouseTests.m
+++ ios/PioneHouseTests/PioneHouseTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SavelokaTests : XCTestCase
+@interface PioneHouseTests : XCTestCase
 
 @end
 
-@implementation SavelokaTests
+@implementation PioneHouseTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
