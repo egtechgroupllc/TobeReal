7a71f47fc5ae75dfe7d1d9b3d4612e5904bed357
baoeggroup <bao@e-ggroup.com>
2025-03-04 15:41:23 +0700

    "update"

--- src/Model/url.js
+++ src/Model/url.js
@@ -1,6 +1,6 @@
 // export const baseUrl = 'https://shome.largeswap.com';
 // export const baseUrl = 'http://192.168.1.16:3000';
-export const baseUrl = 'https://api.saveloka.com';
+export const baseUrl = 'https://apisaveloka.tocuna.com';
 export const KEY_MAP = 'AIzaSyA0p4aVomqYcT6mgNv1h169myUhT7mNE28';
 export const languageUrlServer = `${baseUrl}/language/server`;
 export const languageUrlApp = `${baseUrl}/language/app`;
